e7b0507fbe8ecaf065e50afeaaa0dee36e128dc6
hdonato <donatohugo13@yahoo.com>
2015-10-20 17:04:18 -0300

    c

--- romaneo.unificado/src/main/java/romaneo/unificado/services/AcondicionadorService.java
+++ romaneo.unificado/src/main/java/romaneo/unificado/services/AcondicionadorService.java
@@ -3,27 +3,32 @@ package romaneo.unificado.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import entidades.Acondicionador;
+import romaneo.unificado.entities.Acondicionador;
+import romaneo.unificado.entities.Contacto;
+import romaneo.unificado.entities.Persona;
 
-public class AcondicionadorService
-{
+public class AcondicionadorService {
+	
+	public static List<Acondicionador> allAcondicionadores() {
+		
+		List<Acondicionador> acondicionadores = new ArrayList<>();
 
-	public static List<Acondicionador> allAcondicionadores()
-	{
-		List<Acondicionador> acondicionadores = new ArrayList<Acondicionador>();
-		acondicionadores.add(new Acondicionador("Jorge", "Di Pasqua", "37327466", "jenkins 37"));
-		acondicionadores.add(new Acondicionador("Hugo", "Donato", "29283223", "fente al aeropueto"));
-		acondicionadores.add(new Acondicionador("Felipe", "Carou", "34922443", "Morgan"));
+		for (int i = 0; i < 10; i++) {
+			Contacto contacto = new Contacto("email" + i + "@hotmail.com", "456486" + i);
+			Persona persona = new Persona("Nombre " + i, "Apellido " + i, i);
+
+			Acondicionador acondicionador = new Acondicionador(persona, contacto);
+
+			acondicionadores.add(acondicionador);
+		}
 		return acondicionadores;
 	}
-	
-	public static List<Acondicionador> buscarNombre(String text)
-	{
+
+	public static List<Acondicionador> buscarNombre(String text) {
 		return null;
 	}
 
-	public static List<Acondicionador> bucarApellido(String text)
-	{
+	public static List<Acondicionador> bucarApellido(String text) {
 		return null;
 	}
 
