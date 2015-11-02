569104cb79ba3ed57614002eb5531464b1d7bd05
Felipe <pipecarou@hotmail.com>
2015-11-02 20:48:42 -0300

    prueba

---
+++ romaneo.unificado/src/main/java/romaneo/unificado/rest/AcondicionadoresRest.java
@@ -0,0 +1,11 @@
+package romaneo.unificado.rest;
+
+import javax.ws.rs.Path;
+
+import romaneo.utileria.Constantes;
+
+@Path(Constantes.ConstAcondicionador.PATH)
+public class AcondicionadoresRest
+{
+
+}
