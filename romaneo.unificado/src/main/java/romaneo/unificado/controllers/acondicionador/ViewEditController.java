22c5a8ab019b74dbf234cfda5573add8a218e7e0
poximan <donatohugo13@gmail.com>
2015-11-16 23:41:53 -0300

    modificacion vista para reutilizar servicios daos existentes.

--- romaneo.unificado/src/main/java/romaneo/unificado/controllers/acondicionador/ViewEditController.java
+++ romaneo.unificado/src/main/java/romaneo/unificado/controllers/acondicionador/ViewEditController.java
@@ -1,57 +1,72 @@
 package romaneo.unificado.controllers.acondicionador;
 
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Execution;
-import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Sessions;
-import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
+import romaneo.unificado.controllers.BaseFormController;
 import romaneo.unificado.domain.Acondicionador;
+import romaneo.unificado.services.acondicionador.AcondicionadorService;
 
-public class ViewEditController extends SelectorComposer<Component>
-{
-	/**
-	 * 
-	 */
+public class ViewEditController extends BaseFormController {
+
 	private static final long serialVersionUID = 1L;
+
 	private Acondicionador acondicionador;
-	
-	
-	@Override
-	public void doAfterCompose(Component comp) throws Exception
-	{
-		// TODO Auto-generated method stub
-		super.doAfterCompose(comp);
-		acondicionador = (Acondicionador) Sessions.getCurrent().getAttribute("selected");
-		System.out.print("id: "+Executions.getCurrent().getParameter("id"));
-		
-	}
-	
+
+	@Wire
+	private Window acondicionadorFormWndw;
+
 	@Wire
 	Textbox nombreTxtbx;
-	
-	
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		// TODO Auto-generated method stub
+		super.doAfterCompose(comp);
+
+		acondicionador = (Acondicionador) Sessions.getCurrent().getAttribute("selected");
+
+		/*
+		 * el metodo find lo vas a encontrar en BaseService(), el parametro de
+		 * la funcion Entity find(Object key) se castea segun la implementacion,
+		 * en el caso de acondicionador, key termina siendo un Integer
+		 */
+		Integer un_id_conocido_de_acondicionador = new Integer(1);
+		getService().find(un_id_conocido_de_acondicionador);
+	}
+
 	@Listen("onClick = #btnGuardar")
-	public void guardar()
-	{
+	public void guardar() {
 		nombreTxtbx.setText("chupalaaaaaa..!");
 		System.out.println("btnGuardar...!");
 	}
-	
+
 	@Listen("onClick = #btnEditar")
-	public void editar()
-	{
+	public void editar() {
 		System.out.println("btnEditar...!");
 	}
-	
+
 	@Listen("onClick = #btnVolver")
-	public void volver()
-	{
+	public void volver() {
 		System.out.println("btnVolver...!");
 	}
-	
+
+	@Override
+	public String getClassName() {
+		return Acondicionador.class.getName();
+	}
+
+	@Override
+	protected String getEntityService() {
+		return AcondicionadorService.class.getSimpleName();
+	}
+
+	@Override
+	protected Window getWindowComponent() {
+		return acondicionadorFormWndw;
+	}
 }
